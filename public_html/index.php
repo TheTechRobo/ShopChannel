5149f8aae1c442835e33c31e860cb687886114fa
Spotlight <spotlight@joscomputing.space>
2018-03-25 12:03:21 -0500

    Add background sample

--- public_html/index.php
+++ public_html/index.php
@@ -1,1 +1,14 @@
-oh look a filler page
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"
+        "http://www.w3.org/TR/html4/strict.dtd">
+<HTML>
+<HEAD>
+    <TITLE>Sample page</TITLE>
+</HEAD>
+<BODY>
+Hello, welcome to this HTML 4.01 document.
+<script type="text/JavaScript">
+    var snd = new wiiSound();
+    snd.playBGM();
+</script>
+</BODY>
+</HTML>
