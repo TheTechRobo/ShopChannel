fd175d3013b8da67d9b15203510fe6ae725c4831
Spotlight <spotlight@joscomputing.space>
2018-03-25 12:06:55 -0500

    Move to parent directory for aliasing

--- deploy.php
+++ deploy.php
@@ -2,7 +2,7 @@
 // Below adapted from https://github.com/antriver/auto-git-pull/blob/a4bd39ec03f78d2414b4fc3d45b80261dd846d70/deploy.example.php
 
 use Tmd\AutoGitPull\Deployer;
-require '../vendor/autoload.php';
+require 'vendor/autoload.php';
 
 // TODO: make this dynamic.
 // https://api.github.com/meta
@@ -12,6 +12,5 @@ $deployer = new Deployer([
           '192.30.252.0/22',
           '185.199.108.0/22',
         ],
-        'pullScriptPath' => __DIR__ . '../vendor/tmd/auto-git-pull/scripts/git-pull.sh',
     ]);
 $deployer->deploy();
