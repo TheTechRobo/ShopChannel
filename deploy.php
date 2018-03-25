36d0c4fce6f4f29710bf8740b7bf427dd7bcbf65
Spotlight <spotlight@joscomputing.space>
2018-03-25 12:27:50 -0500

    Yet another useless commit to test automated pulling

--- deploy.php
+++ deploy.php
@@ -1,5 +1,5 @@
 <?php
-// Below adapted from https://github.com/antriver/auto-git-pull/blob/a4bd39ec03f78d2414b4fc3d45b80261dd846d70/deploy.example.php
+// Below is adapted from https://github.com/antriver/auto-git-pull/blob/a4bd39ec03f78d2414b4fc3d45b80261dd846d70/deploy.example.php
 
 use Tmd\AutoGitPull\Deployer;
 require 'vendor/autoload.php';
