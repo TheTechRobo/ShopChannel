4a8c4af651d0b7c13090eb684d389f855368ef29
dhtdht020 <dhtdht020@gmail.com>
2018-03-26 15:55:17 +0300

    Update S_02.php

--- public_html/S_02.php
+++ public_html/S_02.php
@@ -24,7 +24,6 @@
 require_once 'http://spot.norment.in.nf/spotstoreset.php';
     // Close connection
     mysqli_close($link);
-}
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
