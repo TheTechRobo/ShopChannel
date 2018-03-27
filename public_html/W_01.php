a7b925493dee15e4148e225c25a6fef7576e5eca
CornierKhan1 <jordyalexlyall@gmail.com>
2018-03-27 23:55:08 +1100

    W_01.php DO NOT EDIT THIS FILE YET
    DO NOT EDIT, I NEED TO BACK IT UP.

---
+++ public_html/W_01.php
@@ -0,0 +1,114 @@
+<!--Do not modify this file yet, I still need to do something with it before you can butcher it.
+DO NOT EDIT YET, I NEED TO BACK IT UP FIRST, DO NOT EDIT, I NEED TO BACK IT UP.-->
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+<meta content="en-us" http-equiv="Content-Language" />
+<meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
+<title>Open Shop Channel</title>
+<style type="text/css">
+.auto-style1 {
+	font-size: xx-large;
+	text-align: center;
+}
+.auto-style2 {
+	font-size: large;
+}
+.auto-style3 {
+	color: #FFFFFF;
+	background-color: #808080;
+}
+.auto-style4 {
+	border-right-style: solid;
+	border-right-width: 1px;
+	border-top-style: solid;
+	border-top-width: 1px;
+}
+.auto-style5 {
+	border-left-style: solid;
+	border-left-width: 1px;
+	border-right-style: solid;
+	border-right-width: 1px;
+	border-top-style: solid;
+	border-top-width: 1px;
+}
+.auto-style6 {
+	border-left-style: solid;
+	border-left-width: 1px;
+	border-top-style: solid;
+	border-top-width: 1px;
+}
+.auto-style7 {
+	border-right-style: solid;
+	border-right-width: 1px;
+	border-bottom-style: solid;
+	border-bottom-width: 1px;
+}
+.auto-style8 {
+	border-left-style: solid;
+	border-left-width: 1px;
+	border-right-style: solid;
+	border-right-width: 1px;
+	border-bottom-style: solid;
+	border-bottom-width: 1px;
+}
+.auto-style9 {
+	border-left-style: solid;
+	border-left-width: 1px;
+	border-bottom-style: solid;
+	border-bottom-width: 1px;
+}
+.auto-style10 {
+	text-align: left;
+}
+.auto-style11 {
+	color: #C0C0C0;
+	text-align: center;
+}
+.auto-style12 {
+	color: #800000;
+	text-align: center;
+	font-size: x-large;
+}
+.auto-style13 {
+	color: #008000;
+	text-align: center;
+	font-size: x-large;
+}
+</style>
+</head>
+
+<body style="margin: 0">
+<!--Do not modify this file yet, I still need to do something with it before you can butcher it.
+DO NOT EDIT YET, I NEED TO BACK IT UP FIRST, DO NOT EDIT, I NEED TO BACK IT UP.-->
+
+<div id="layer1" class="auto-style1" style="position: absolute; width: 640px; height: 480px; z-index: 1; left: 3px; top: 3px">
+	Open Shop Channel<span class="auto-style2"> <span class="auto-style3">&nbsp;Featured 
+	Homebrew </span><br />
+	<table align="center" style="width: 97%; height: 228px">
+		<tr>
+			<td class="auto-style7">1</td>
+			<td class="auto-style8">2</td>
+			<td class="auto-style9">3</td>
+		</tr>
+		<tr>
+			<td class="auto-style4">4</td>
+			<td class="auto-style5">5</td>
+			<td class="auto-style6">6</td>
+		</tr>
+	</table>
+	<hr />
+	</span>
+	<div class="auto-style10">
+		<p class="auto-style11">Explore Open Shop</p>
+		<p class="auto-style12"><strong>Tools</strong></p>
+		<p class="auto-style13"><strong>Games</strong></p>
+	</div>
+</div>
+<!--Do not modify this file yet, I still need to do something with it before you can butcher it.
+DO NOT EDIT YET, I NEED TO BACK IT UP FIRST, DO NOT EDIT, I NEED TO BACK IT UP.-->
+
+</body>
+
+</html>
