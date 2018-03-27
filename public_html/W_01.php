43d8d77b2f30ace925c554c561795c85f74dc0ee
CornierKhan1 <jordyalexlyall@gmail.com>
2018-03-28 00:26:36 +1100

    GIF Image Buttons?

--- public_html/W_01.php
+++ public_html/W_01.php
@@ -68,14 +68,34 @@
 .auto-style13 {
 	text-align: center;
 }
-.auto-style14 {
-	color: #FFFFFF;
-	background-color: #0000FF;
-}
 </style>
+<script type="text/javascript">
+<!--
+function FP_preloadImgs() {//v1.0
+ var d=document,a=arguments; if(!d.FP_imgs) d.FP_imgs=new Array();
+ for(var i=0; i<a.length; i++) { d.FP_imgs[i]=new Image; d.FP_imgs[i].src=a[i]; }
+}
+
+function FP_swapImg() {//v1.0
+ var doc=document,args=arguments,elm,n; doc.$imgSwaps=new Array(); for(n=2; n<args.length;
+ n+=2) { elm=FP_getObjectByID(args[n]); if(elm) { doc.$imgSwaps[doc.$imgSwaps.length]=elm;
+ elm.$src=elm.src; elm.src=args[n+1]; } }
+}
+
+function FP_getObjectByID(id,o) {//v1.0
+ var c,el,els,f,m,n; if(!o)o=document; if(o.getElementById) el=o.getElementById(id);
+ else if(o.layers) c=o.layers; else if(o.all) el=o.all[id]; if(el) return el;
+ if(o.id==id || o.name==id) return o; if(o.childNodes) c=o.childNodes; if(c)
+ for(n=0; n<c.length; n++) { el=FP_getObjectByID(id,c[n]); if(el) return el; }
+ f=o.forms; if(f) for(n=0; n<f.length; n++) { els=f[n].elements;
+ for(m=0; m<els.length; m++){ el=FP_getObjectByID(id,els[n]); if(el) return el; } }
+ return null;
+}
+// -->
+</script>
 </head>
 
-<body style="margin: 0">
+<body style="margin: 0" onload="FP_preloadImgs(/*url*/'img/button/exploreA.gif',/*url*/'img/button/exploreB.gif')">
 
 <div class="auto-style13">
 	<span class="auto-style12"><strong>Open Shop Channel</strong></span><span class="auto-style2">
@@ -96,8 +116,8 @@
 </table>
 <hr /></span>
 <div class="auto-style10">
-	<h1 class="auto-style13"><strong><span class="auto-style14">&nbsp;Explore 
-	Now </span></strong></h1>
+	<h1 class="auto-style13">
+	<img id="img1" alt="Explore Now" height="30" onmousedown="FP_swapImg(1,0,/*id*/'img1',/*url*/'img/button/exploreB.gif')" onmouseout="FP_swapImg(0,0,/*id*/'img1',/*url*/'img/button/exploreC.gif')" onmouseover="FP_swapImg(1,0,/*id*/'img1',/*url*/'img/button/exploreA.gif')" onmouseup="FP_swapImg(0,0,/*id*/'img1',/*url*/'img/button/exploreA.gif')" src="img/button/exploreC.gif" style="border: 0" width="150" /><!-- MSComment="ibutton" fp-style="fp-btn: Soft Rectangle 6; fp-font: Yu Gothic UI Light; fp-font-size: 14; fp-transparent: 1" fp-title="Explore Now" --></h1>
 </div>
 
 </body>
