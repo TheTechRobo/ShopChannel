7a1dd1c786ace3c7c8824592faee0457b01b9de7
CornierKhan1 <jordyalexlyall@gmail.com>
2018-03-28 16:28:56 +1100

    Optimized, Improved compatibility + Explore
    Updated the W_01.php in prep for the W_02.php

---
+++ public_html/homebrew/H_01.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+<meta content="en-us" http-equiv="Content-Language" />
+<meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
+<title>Homebrew on The Open Shop</title>
+<style type="text/css">
+.auto-style1 {
+	text-align: left;
+	margin-left: 40px;
+}
+.auto-style2 {
+	text-align: center;
+}
+</style>
+<script type="text/javascript">
+<!--
+function FP_swapImg() {//v1.0
+ var doc=document,args=arguments,elm,n; doc.$imgSwaps=new Array(); for(n=2; n<args.length;
+ n+=2) { elm=FP_getObjectByID(args[n]); if(elm) { doc.$imgSwaps[doc.$imgSwaps.length]=elm;
+ elm.$src=elm.src; elm.src=args[n+1]; } }
+}
+
+function FP_preloadImgs() {//v1.0
+ var d=document,a=arguments; if(!d.FP_imgs) d.FP_imgs=new Array();
+ for(var i=0; i<a.length; i++) { d.FP_imgs[i]=new Image; d.FP_imgs[i].src=a[i]; }
+}
+
+function FP_getObjectByID(id,o) {//v1.0
+ var c,el,els,f,m,n; if(!o)o=document; if(o.getElementById) el=o.getElementById(id);
+ else if(o.layers) c=o.layers; else if(o.all) el=o.all[id]; if(el) return el;
+ if(o.id==id || o.name==id) return o; if(o.childNodes) c=o.childNodes; if(c)
+ for(n=0; n<c.length; n++) { el=FP_getObjectByID(id,c[n]); if(el) return el; }
+ f=o.forms; if(f) for(n=0; n<f.length; n++) { els=f[n].elements;
+ for(m=0; m<els.length; m++){ el=FP_getObjectByID(id,els[n]); if(el) return el; } }
+ return null;
+}
+// -->
+</script>
+</head>
+
+<body onload="FP_preloadImgs(/*url*/'../img/button/gamesA.gif',/*url*/'../img/button/gamesB.gif',/*url*/'../img/button/ToolsA.gif',/*url*/'../img/button/ToolsB.gif',/*url*/'../img/button/return2a.gif',/*url*/'../img/button/return2b.gif')" style="background-image: url('../img/hbback.gif'); background-attachment: fixed">
+
+<div class="auto-style2">
+	<div class="auto-style2">
+		<p>&nbsp;</p>
+		<p>&nbsp;</p>
+		<p class="auto-style1">Homebrew is sorted into two categories. Tools and 
+		Games.</p>
+		<p class="auto-style2"><a href="G_01.php">
+		<img id="img1" alt="Games" height="46" onmousedown="FP_swapImg(1,0,/*id*/'img1',/*url*/'../img/button/gamesB.gif')" onmouseout="FP_swapImg(0,0,/*id*/'img1',/*url*/'../img/button/gamesC.gif')" onmouseover="FP_swapImg(1,0,/*id*/'img1',/*url*/'../img/button/gamesA.gif')" onmouseup="FP_swapImg(0,0,/*id*/'img1',/*url*/'../img/button/gamesA.gif')" src="../img/button/gamesC.gif" style="border: 0" width="230" /><!-- MSComment="ibutton" fp-style="fp-btn: Soft Rectangle 6; fp-font: Yu Gothic UI Semibold; fp-font-size: 16; fp-font-color-hover: #C0C0C0; fp-transparent: 1" fp-title="Games" --></a></p>
+		<a href="T_01.php">
+		<img id="img2" alt="Tools" height="46" onmousedown="FP_swapImg(1,0,/*id*/'img2',/*url*/'../img/button/ToolsB.gif')" onmouseout="FP_swapImg(0,0,/*id*/'img2',/*url*/'../img/button/ToolsC.gif')" onmouseover="FP_swapImg(1,0,/*id*/'img2',/*url*/'../img/button/ToolsA.gif')" onmouseup="FP_swapImg(0,0,/*id*/'img2',/*url*/'../img/button/ToolsA.gif')" src="../img/button/ToolsC.gif" style="border: 0" width="230" /><!-- MSComment="ibutton" fp-style="fp-btn: Soft Rectangle 6; fp-font: Yu Gothic UI Semibold; fp-font-size: 16; fp-font-color-hover: #C0C0C0; fp-transparent: 1" fp-title="Tools" --></a><br />
+	</div>
+	<a href="../W_01.php">
+<img id="img3" alt="Return" height="40" onmousedown="FP_swapImg(1,0,/*id*/'img3',/*url*/'../img/button/return2b.gif')" onmouseout="FP_swapImg(0,0,/*id*/'img3',/*url*/'../img/button/return2c.gif')" onmouseover="FP_swapImg(1,0,/*id*/'img3',/*url*/'../img/button/return2a.gif')" onmouseup="FP_swapImg(0,0,/*id*/'img3',/*url*/'../img/button/return2a.gif')" src="../img/button/return2c.gif" style="border: 0" width="200" /><!-- MSComment="ibutton" fp-style="fp-btn: Soft Rectangle 1; fp-font: Yu Gothic UI Semibold; fp-font-size: 14; fp-font-color-normal: #808080; fp-font-color-hover: #C0C0C0; fp-transparent: 1" fp-title="Return" --></a></div>
+
+</body>
+
+</html>
