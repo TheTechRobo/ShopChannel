113cf5f8c4b2644580da539acc3023215b924e2d
dhtdht020 <dhtdht020@gmail.com>
2018-03-28 09:21:10 +0300

    Create flashtest.php

---
+++ public_html/flashtest.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<object type="application/x-shockwave-flash" data="../dc24mail.swf" 
+id="applicationID" style="margin:0 10px;width:auto;height:auto;">
+
+<param name="movie" value="../dc24mail.swf" />
+<param name="wmode" value="transparent" /> <!-- Or opaque, etc. -->
+
+<!-- ↓ Required paramter or not, depends on application -->
+<param name="FlashVars" value="" />
+
+<param name="quality" value="high" />
+<param name="menu" value="false" />
+
+</object>
+</html>
