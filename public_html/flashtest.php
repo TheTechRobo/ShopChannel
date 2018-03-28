2b7f954f9488f141d218a1e32ea78a49d8fe2aad
CornierKhan1 <jordyalexlyall@gmail.com>
2018-03-28 17:26:57 +1100

    Edited

--- public_html/flashtest.php
+++ public_html/flashtest.php
@@ -1,16 +1,14 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
 <html>
-<object type="application/x-shockwave-flash" data="../dc24mail.swf" 
-id="applicationID" style="margin:0 10px;width:auto;height:auto;">
-
-<param name="movie" value="../dc24mail.swf" />
-<param name="wmode" value="transparent" /> <!-- Or opaque, etc. -->
-
-<!-- ↓ Required paramter or not, depends on application -->
-<param name="FlashVars" value="" />
-
-<param name="quality" value="high" />
-<param name="menu" value="false" />
-
+<body>
+<object id="applicationID" data="../dc24mail.swf" style="margin: 0 10px; width: 497px; height: 365px;" type="application/x-shockwave-flash">
+	<param name="movie" value="../dc24mail.swf">
+	<param name="wmode" value="transparent">
+	<!-- Or opaque, etc. -->  
+	<!-- ↓ Required paramter or not, depends on application --> 
+	<param name="FlashVars" value="">
+	<param name="quality" value="high">
+	<param name="menu" value="false">
 </object>
+</body>
 </html>
