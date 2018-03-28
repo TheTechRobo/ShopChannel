5da4675bedc2296a5a8bdd7b0ea4eef0634bda57
CornierKhan1 <jordyalexlyall@gmail.com>
2018-03-28 17:15:17 +1100

    swf, style fixes

--- public_html/W_02.php
+++ public_html/W_02.php
@@ -5,18 +5,21 @@
 <meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
 <title>Explore Open Shop</title>
 <style type="text/css">
-.auto-style12 {
+.pt-36 {
 	font-size: xx-large;
 }
-.auto-style2 {
+.pt-18 {
 	font-size: large;
 }
-.auto-style3 {
+.highlighter {
 	color: #FFFFFF;
 	background-color: #34BEED;
 }
-.auto-style13 {
+.centeralignment {
 	text-align: center;
+}
+.leftalignment {
+	text-align: left;
 }
 </style>
 <script type="text/javascript">
@@ -41,22 +44,22 @@ function FP_getObjectByID(id,o) {//v1.0
  for(m=0; m<els.length; m++){ el=FP_getObjectByID(id,els[n]); if(el) return el; } }
  return null;
 }
-// -->
+//-->
 </script>
 </head>
 
+
 <body onload="FP_preloadImgs(/*url*/'img/button/returnE.gif',/*url*/'img/button/returnF.gif')">
 
-<p class="auto-style13"><span class="auto-style12"><strong>Open Shop Channel</strong></span><span class="auto-style2">
-	<span class="auto-style3">&nbsp;Explore </span></span></p>
-<p class="auto-style13">Select a Store</p>
-<p class="auto-style13"><a href="homebrew/H_01.php">
+<p class="centeralignment"><span class="pt-36"><strong>Open Shop Channel</strong></span><span class="pt-18">
+	<span class="highlighter">&nbsp;Explore </span></span></p>
+<p class="centeralignment"><a href="homebrew/H_01.php">
 <img alt="" height="50" src="img/Store%20Icons/homebrew.png" width="100" /></a>&nbsp;
 <img alt="" height="50" src="img/Store%20Icons/patchingstore.png" width="100" />&nbsp;
 <img alt="" height="50" src="img/Store%20Icons/clubspot.png" width="100" /></p>
-<p class="auto-style13">Enjoy Homebrew, Patches and Rewards on The Open Shop 
-Channel!</p>
-<p class="auto-style13"><a href="W_01.php">
+<h3><strong>Latest Additions</strong></h3>
+<p class="leftalignment"><strong>Name </strong><em>Added 28/03/2018&nbsp; </em></p>
+<p class="centeralignment"><a href="W_01.php">
 <img id="img1" alt="Return" height="40" onmousedown="FP_swapImg(1,0,/*id*/'img1',/*url*/'img/button/returnF.gif')" onmouseout="FP_swapImg(0,0,/*id*/'img1',/*url*/'img/button/returnD.gif')" onmouseover="FP_swapImg(1,0,/*id*/'img1',/*url*/'img/button/returnE.gif')" onmouseup="FP_swapImg(0,0,/*id*/'img1',/*url*/'img/button/returnE.gif')" src="img/button/returnD.gif" style="border: 0" width="200" /><!-- MSComment="ibutton" fp-style="fp-btn: Soft Rectangle 1; fp-font: Yu Gothic UI Semibold; fp-font-size: 14; fp-font-color-normal: #808080; fp-font-color-hover: #C0C0C0; fp-transparent: 1" fp-title="Return" --></a></p>
 
 </body>
