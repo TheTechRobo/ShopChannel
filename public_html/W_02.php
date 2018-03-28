0bbbe5abe6a44f518af88ae226b1aa9db7dadc2c
CornierKhan1 <jordyalexlyall@gmail.com>
2018-03-28 16:30:18 +1100

    An Explore Prototype

--- public_html/W_02.php
+++ public_html/W_02.php
@@ -19,15 +19,45 @@
 	text-align: center;
 }
 </style>
+<script type="text/javascript">
+<!--
+function FP_swapImg() {//v1.0
+ var doc=document,args=arguments,elm,n; doc.$imgSwaps=new Array(); for(n=2; n<args.length;
+ n+=2) { elm=FP_getObjectByID(args[n]); if(elm) { doc.$imgSwaps[doc.$imgSwaps.length]=elm;
+ elm.$src=elm.src; elm.src=args[n+1]; } }
+}
+
+function FP_preloadImgs() {//v1.0
+ var d=document,a=arguments; if(!d.FP_imgs) d.FP_imgs=new Array();
+ for(var i=0; i<a.length; i++) { d.FP_imgs[i]=new Image; d.FP_imgs[i].src=a[i]; }
+}
+
+function FP_getObjectByID(id,o) {//v1.0
+ var c,el,els,f,m,n; if(!o)o=document; if(o.getElementById) el=o.getElementById(id);
+ else if(o.layers) c=o.layers; else if(o.all) el=o.all[id]; if(el) return el;
+ if(o.id==id || o.name==id) return o; if(o.childNodes) c=o.childNodes; if(c)
+ for(n=0; n<c.length; n++) { el=FP_getObjectByID(id,c[n]); if(el) return el; }
+ f=o.forms; if(f) for(n=0; n<f.length; n++) { els=f[n].elements;
+ for(m=0; m<els.length; m++){ el=FP_getObjectByID(id,els[n]); if(el) return el; } }
+ return null;
+}
+// -->
+</script>
 </head>
 
-<body>
+<body onload="FP_preloadImgs(/*url*/'img/button/returnE.gif',/*url*/'img/button/returnF.gif')">
 
 <p class="auto-style13"><span class="auto-style12"><strong>Open Shop Channel</strong></span><span class="auto-style2">
 	<span class="auto-style3">&nbsp;Explore </span></span></p>
-<p class="auto-style13">We are currently working hard to bring you homebrew.<br />
-Unfortunately, we still need to get some.</p>
-<p class="auto-style13"><a href="W_01.php">Back</a></p>
+<p class="auto-style13">Select a Store</p>
+<p class="auto-style13"><a href="homebrew/H_01.php">
+<img alt="" height="50" src="img/Store%20Icons/homebrew.png" width="100" /></a>&nbsp;
+<img alt="" height="50" src="img/Store%20Icons/patchingstore.png" width="100" />&nbsp;
+<img alt="" height="50" src="img/Store%20Icons/clubspot.png" width="100" /></p>
+<p class="auto-style13">Enjoy Homebrew, Patches and Rewards on The Open Shop 
+Channel!</p>
+<p class="auto-style13"><a href="W_01.php">
+<img id="img1" alt="Return" height="40" onmousedown="FP_swapImg(1,0,/*id*/'img1',/*url*/'img/button/returnF.gif')" onmouseout="FP_swapImg(0,0,/*id*/'img1',/*url*/'img/button/returnD.gif')" onmouseover="FP_swapImg(1,0,/*id*/'img1',/*url*/'img/button/returnE.gif')" onmouseup="FP_swapImg(0,0,/*id*/'img1',/*url*/'img/button/returnE.gif')" src="img/button/returnD.gif" style="border: 0" width="200" /><!-- MSComment="ibutton" fp-style="fp-btn: Soft Rectangle 1; fp-font: Yu Gothic UI Semibold; fp-font-size: 14; fp-font-color-normal: #808080; fp-font-color-hover: #C0C0C0; fp-transparent: 1" fp-title="Return" --></a></p>
 
 </body>
 
