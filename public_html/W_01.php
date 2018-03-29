0a3e38b18dea86154d7cbe5d5c3f50beb83c1f5c
CornierKhan1 <jordyalexlyall@gmail.com>
2018-03-29 15:10:11 +1100

    All the fixes

--- public_html/W_01.php
+++ public_html/W_01.php
@@ -2,124 +2,31 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 
 <head>
-<meta content="en-us" http-equiv="Content-Language" />
-<meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
-<title>Open Shop Channel</title>
-<style type="text/css">
-.auto-style2 {
-	font-size: large;
-}
-.auto-style3 {
-	color: #FFFFFF;
-	background-color: #34BEED;
-}
-.auto-style7 {
-	border-right-style: solid;
-	border-right-width: 1px;
-	border-bottom-style: solid;
-	border-bottom-width: 1px;
-	text-align: center;
-}
-.auto-style8 {
-	border-left-style: solid;
-	border-left-width: 1px;
-	border-right-style: solid;
-	border-right-width: 1px;
-	border-bottom-style: solid;
-	border-bottom-width: 1px;
-	text-align: center;
-}
-.auto-style9 {
-	border-left-style: solid;
-	border-left-width: 1px;
-	border-bottom-style: solid;
-	border-bottom-width: 1px;
-	text-align: center;
-}
-.auto-style4 {
-	border-right-style: solid;
-	border-right-width: 1px;
-	border-top-style: solid;
-	border-top-width: 1px;
-	text-align: center;
-}
-.auto-style5 {
-	border-left-style: solid;
-	border-left-width: 1px;
-	border-right-style: solid;
-	border-right-width: 1px;
-	border-top-style: solid;
-	border-top-width: 1px;
-	text-align: center;
-}
-.auto-style6 {
-	border-left-style: solid;
-	border-left-width: 1px;
-	border-top-style: solid;
-	border-top-width: 1px;
-	text-align: center;
-}
-.auto-style10 {
-	text-align: left;
-}
-.auto-style12 {
-	font-size: xx-large;
-}
-.auto-style13 {
-	text-align: center;
-}
-</style>
-<script type="text/javascript">
-<!--
-function FP_preloadImgs() {//v1.0
- var d=document,a=arguments; if(!d.FP_imgs) d.FP_imgs=new Array();
- for(var i=0; i<a.length; i++) { d.FP_imgs[i]=new Image; d.FP_imgs[i].src=a[i]; }
-}
-
-function FP_swapImg() {//v1.0
- var doc=document,args=arguments,elm,n; doc.$imgSwaps=new Array(); for(n=2; n<args.length;
- n+=2) { elm=FP_getObjectByID(args[n]); if(elm) { doc.$imgSwaps[doc.$imgSwaps.length]=elm;
- elm.$src=elm.src; elm.src=args[n+1]; } }
-}
-
-function FP_getObjectByID(id,o) {//v1.0
- var c,el,els,f,m,n; if(!o)o=document; if(o.getElementById) el=o.getElementById(id);
- else if(o.layers) c=o.layers; else if(o.all) el=o.all[id]; if(el) return el;
- if(o.id==id || o.name==id) return o; if(o.childNodes) c=o.childNodes; if(c)
- for(n=0; n<c.length; n++) { el=FP_getObjectByID(id,c[n]); if(el) return el; }
- f=o.forms; if(f) for(n=0; n<f.length; n++) { els=f[n].elements;
- for(m=0; m<els.length; m++){ el=FP_getObjectByID(id,els[n]); if(el) return el; } }
- return null;
-}
-// -->
-</script>
+	<link href="main.css" rel="stylesheet" type="text/css"/>
+	<script type="text/javascript" src="js/button.js"></script>
+	<script type="text/javascript" src="js/wiicore.js"></script>
 </head>
-
-<body onload="FP_preloadImgs(/*url*/'img/button/exploreA.gif',/*url*/'img/button/exploreB.gif')" style="margin: 0">
-
-<div class="auto-style13">
-	<span class="auto-style12"><strong><br />
-	Open Shop Channel</strong></span><span class="auto-style2">
-	<span class="auto-style3">&nbsp;Featured Homebrew </span><br />
-	<table align="center" style="width: 97%; height: 228px">
-		<tr>
-			<td class="auto-style7">1</td>
-			<td class="auto-style8">2</td>
-			<td class="auto-style9" style="width: 165px">3</td>
-		</tr>
-		<tr>
-			<td class="auto-style4">4</td>
-			<td class="auto-style5">5</td>
-			<td class="auto-style6" style="width: 165px">6</td>
-		</tr>
-	</table>
-	<hr /></span>
-	<div class="auto-style10">
-		<h1 class="auto-style13"><a href="W_02.php">
-		<img id="img1" alt="Explore Now" height="30" onmousedown="FP_swapImg(1,0,/*id*/'img1',/*url*/'img/button/exploreB.gif')" onmouseout="FP_swapImg(0,0,/*id*/'img1',/*url*/'img/button/exploreC.gif')" onmouseover="FP_swapImg(1,0,/*id*/'img1',/*url*/'img/button/exploreA.gif')" onmouseup="FP_swapImg(0,0,/*id*/'img1',/*url*/'img/button/exploreA.gif')" src="img/button/exploreC.gif" style="border: 0" width="150" /></a></h1>
-	</div>
-</div>
-
-</body>
-
+	<body>
+		<p class="heading">Open Shop Channel<span class="highlighted"> Featured </span></p>
+		
+			<!--The featured Homebrew of the (INSERT PERIOD OF TIME)!-->
+				<table style="width: 100%; height: 125px" class="table">
+					<tr>
+						<td>1</td>
+						<td>2</td>
+						<td>3</td>
+					</tr>
+					<tr>
+						<td>4</td>
+						<td>5</td>
+						<td>6</td>
+					</tr>
+				</table>
+			<!--For the sake of order, stylize Buttons with the heading class in main.css.-->
+		<p class="heading">
+			<img alt="Explore Button" id="explore" dir="ltr" height="100" src="img/button/exploreA.png" width="200" onmousedown="FP_swapImg(1,1,/*id*/'explore',/*url*/'img/button/exploreC.png')" onmouseout="FP_swapImgRestore()" onmouseover="FP_swapImg(1,1,/*id*/'explore',/*url*/'img/button/exploreB.png');" onmouseup="FP_swapImgRestore()" class="heading" onclick="FP_goToURL(/*href*/'W_02.php');")>
+			</img>
+			<span class="highlighted"><a href="debug.php">&nbsp;debugmii </a></span>
+		</p>
+	</body>
 </html>
---
+++ public_html/js/wiicore.js
@@ -0,0 +1,7 @@
+﻿/*This file is for quick access, simply use <script src="js/wiicore.js"></script> to load it into any page on public_html.*/
+
+/*It does what it says on the tin, it allows sounds on the Wii to be played.*/
+sound = new wiiSound();
+
+/*Load a keyboard. Needs fixing(?)*/
+keyboard = new wiiKeyboard();
