1fc4f7c368b4ded68f3a662002df63976471fcae
DarkPhoenix10 <thedarkpheonix123@gmail.com>
2018-04-13 17:02:03 +0100

    does sound 8 exist?

--- public_html/soundtest.php
+++ public_html/soundtest.php
@@ -13,6 +13,7 @@
 <button onmousedown="sound.playSE(5)">keyboard: switch</button>
 <button onmousedown="sound.playSE(6)">keyboard: empty</button>
 <button onmousedown="sound.playSE(7)">success?</button>
+<button onmousedown="sound.playSE(8)">unknown 1</button>
 <br>
 <br>
 <a href="index2.php">i wanna go home</a>
