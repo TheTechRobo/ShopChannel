d733047e90927933481e11e9b58454cbdc224620
DarkPhoenix10 <thedarkpheonix123@gmail.com>
2018-04-13 17:30:31 +0100

    document soundsdsdsd

--- public_html/js/wiiSound.js
+++ public_html/js/wiiSound.js
@@ -11,5 +11,16 @@ sound.playSE(id) - play a sound
 4: cancel
 5: keyboard - switch
 6: keyboard - empty
-7: sucess?
+7: dl success?
+8: update success?
+9: mario: jump
+10: mario: jump (low)
+11: mario: fireball
+12: mario: coin
+13: mario: brick block?
+14: mario: squash
+15: loading 1
+16: loading 2
+
+apparently you can stop both loading sounds using the success sounds
 */
