6dd796e20a68174f4cc666ae5cd6c5c417e5e524
DarkPhoenix10 <thedarkpheonix123@gmail.com>
2018-04-13 17:24:01 +0100

    moar sounds

--- public_html/soundtest.php
+++ public_html/soundtest.php
@@ -7,6 +7,8 @@
 <body>
 <button onmousedown="sound.playBGM()">play background music</button>
 <!-- sound 1 for me seems to be nothing, according to the orignal code, sound 1 is "slide" -->
+<button onmousedown="sound.playSE(0)">0</button>
+<button onmousedown="sound.playSE(1)">1</button>
 <button onmousedown="sound.playSE(2)">focus</button>
 <button onmousedown="sound.playSE(3)">select (pressed)</button>
 <button onmousedown="sound.playSE(4)">cancel</button>
@@ -14,16 +16,17 @@
 <button onmousedown="sound.playSE(6)">keyboard: empty</button>
 <button onmousedown="sound.playSE(7)">dl success?</button>
 <button onmousedown="sound.playSE(8)">update success?</button>
-<button onmousedown="sound.playSE(9)">sound 9</button>
-<button onmousedown="sound.playSE(10)">sound 10</button>
-<button onmousedown="sound.playSE(11)">sound 11</button>
-<button onmousedown="sound.playSE(12)">sound 12</button>
-<button onmousedown="sound.playSE(13)">sound 13</button>
-<button onmousedown="sound.playSE(14)">sound 14</button>
-<button onmousedown="sound.playSE(15)">sound 15</button>
-<button onmousedown="sound.playSE(16)">sound 16</button>
-<button onmousedown="sound.playSE(17)">sound 17</button>
-<button onmousedown="sound.playSE(18)">sound 18</button>
+<button onmousedown="sound.playSE(9)">mario: jump</button>
+<button onmousedown="sound.playSE(10)">mario: jump (low)</button>
+<button onmousedown="sound.playSE(11)">mario: fireball</button>
+<button onmousedown="sound.playSE(12)">mario: coin</button>
+<button onmousedown="sound.playSE(13)">mario: brick block?</button>
+<button onmousedown="sound.playSE(14)">mario: squash</button>
+<button onmousedown="sound.playSE(15)">loading 1</button>
+<button onmousedown="sound.playSE(16)">loading 2</button>
+<button onmousedown="sound.playSE(19)">does this exist? 19</button>
+<button onmousedown="sound.playSE(20)">does this exist? 20</button>
+<button onmousedown="sound.playSE(21)">does this exist? 21</button>
 <br>
 <br>
 <a href="index2.php">i wanna go home</a>
