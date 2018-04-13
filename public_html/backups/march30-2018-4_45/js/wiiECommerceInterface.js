a80536a73a6804a6671d8b781c3cc72b0ac875c4
guytwig-PC\guy twig <dhtdht020@gmail.com>
2018-04-13 15:54:18 +0300

    more stuff

--- public_html/backups/march30-2018-4_45/js/wiiECommerceInterface.js
+++
@@ -1,1 +0,0 @@
-var wiiec = new ECommerceInterface(); 
--- public_html/backups/march30-2018-4_45/js/wiicore.js
+++
@@ -1,9 +0,0 @@
-//This file is for quick access, simply use <script src="js/wiicore.js"></script> to load it into any page on public_html.
-var toLoad = ["wiiShop", "wiiKeyboard", "wiiSound", "wiiECommerceInterface"]
-var newScriptLol;
-for (var ok = 0; ok < toLoad.length; ok++) {
-    newScriptLol = document.createElement("script");
-	newScriptLol.type = "text/javascript";
-	newScriptLol.src = "https://openshop.norment.in.nf/js/" + toLoad[ok] + ".js";
-	document.getElementsByTagName("head")[0].appendChild(newScriptLol);
-}
