1585c53ff9697b0afd32f82cb3b5b7b60c31fd11
dhtdht020 <dhtdht020@gmail.com>
2018-04-14 16:15:33 +0300

    output made me blind

--- deploy.php
+++ deploy.php
@@ -33,7 +33,7 @@
     <meta charset="UTF-8">
     <title>GIT DEPLOYMENT SCRIPT</title>
 </head>
-<body style="background-color: #000000; color: #FFFFFF; font-weight: bold; padding: 0 10px;">
+<body style="background-color: #FFFFFF; color: #FFFFFF; font-weight: bold; padding: 0 10px;">
 <div style="width:700px">
     <div style="float:left;width:350px;">
     <p style="color:white;">Git Deployment Script</p>
