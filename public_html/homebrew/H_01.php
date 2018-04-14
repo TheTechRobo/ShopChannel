fe947f82446fec6c6dd2501db648ce3131dceb65
CornierKhan1 <jordyalexlyall@gmail.com>
2018-04-14 16:04:14 +1000

    It's no longer lonely. :octocat:
    Homebrew base made.

--- public_html/homebrew/H_01.php
+++ public_html/homebrew/H_01.php
@@ -2,13 +2,23 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 
 <head>
-<meta content="text/html; charset=utf-8" http-equiv="Content-Type" x-undefined="" />
-<title>Untitled 1</title>
+<meta content="en-us" http-equiv="Content-Language" />
+<meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
+<title>Homebrew</title>
+<link href="../main.css" rel="stylesheet" type="text/css" />
+<style type="text/css">
+</style>
 </head>
 
-<body>
-cornier
-it's lonely here
+<body class="heading">
+
+
+
+<p>Homebrew<span class="highlighted"> Catalogue </span></p>
+<p class="default">Games, Utilities and cool looking Demos!</p>
+
+
+
 </body>
 
 </html>
