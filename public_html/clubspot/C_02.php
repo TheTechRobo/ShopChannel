32359c9692516a7e0e44eef63cea2a1ae8b01481
dhtdht020 <dhtdht020@gmail.com>
2018-04-21 08:03:15 +0300

    Update C_02.php

--- public_html/clubspot/C_02.php
+++ public_html/clubspot/C_02.php
@@ -89,7 +89,6 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         .wrapper{ width: 350px; padding: 20px; }
 	
 	input[type=text] {
-    	box-sizing: border-box;
    	border: 2px solid #009ac7;
 	}
     </style>
