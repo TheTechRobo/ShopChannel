68416dfde9285aa2d38754d59959c065c70a8107
CornierKhan1 <jordyalexlyall@gmail.com>
2018-04-26 20:03:11 +1000

    Credit where credit is due.
    Giving credit where credit is due. :D

---
+++ public_html/docs/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+</head>
+
+<body>
+<p>(This document can be stylised later. We need to focus on other parts first.)</p>
+<p>Button Design: Iyenal</p>
+<p>Frontend: CornierKhan1</p>
+<p>Backend: Dhtdht020 and DarkPheonix</p>
+<div></div>
+<p>Thank you for using The Open Shop!</p>
+</body>
+
+</html>
