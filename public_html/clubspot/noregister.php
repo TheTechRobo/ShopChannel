6163f7ca1c8a627a7e5928908f85ed7c75ce355f
dhtdht020 <dhtdht020@gmail.com>
2018-05-09 17:49:03 +0300

    Changed all URLs to renamed files I guess

--- public_html/clubspot/noregister.php
+++ public_html/clubspot/noregister.php
@@ -14,7 +14,7 @@
 <p style="text-align: center;"><span style="font-size: 20px;">Club Spot Registeration</span></p>
 <p style="text-align: center;">Registeration is not available in the shop. Please access spot.norment.in.nf from your preferred device.</p>
 <p class="heading">
-				<img alt="Return Button" id="returnbut" dir="ltr" height="100" src="../asset/button/returnA.png" width="200" onmousedown="FP_swapImg(1,1,/*id*/'returnbut',/*url*/'../asset/button/returnC.png')" onmouseout="FP_swapImgRestore()" onmouseover="FP_swapImg(1,1,/*id*/'returnbut',/*url*/'../asset/button/returnB.png')" onmouseup="FP_swapImgRestore()" class="heading" onclick="FP_goToURL(/*href*/'../explore.php')")>
+				<img alt="Return Button" id="returnbut" dir="ltr" height="100" src="../asset/button/returnA.png" width="200" onmousedown="FP_swapImg(1,1,/*id*/'returnbut',/*url*/'../asset/button/returnC.png')" onmouseout="FP_swapImgRestore()" onmouseover="FP_swapImg(1,1,/*id*/'returnbut',/*url*/'../asset/button/returnB.png')" onmouseup="FP_swapImgRestore()" class="heading" onclick="FP_goToURL(/*href*/'login.php')")>
 				</img>
 </body>
 </html>
