5c175462bb4f4a809af74ded53e9c5796b0882a5
CornierKhan1 <jordyalexlyall@gmail.com>
2018-05-10 21:31:42 +1000

    Updated Debug.php and added DLTasks.php.

---
+++ public_html/DLTasks.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+    <meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
+    <script src="js/wiicore.js"></script>
+    <script src="js/DownloadTasks.js"></script>
+</head>
+<!--Please don't attach CSS or images to this document, we would rather have this page load rather quickly.-->
+<body>
+<p><button onmousedown="addDownload(1)">play thy bgm</button></p>
+</body>
+
+</html>
--- public_html/debug.php
+++ public_html/debug.php
@@ -14,6 +14,7 @@
 					<p><a href="wiiShop-test.php">test some wiiShop thingies</a></p>
 					<p><a href="othertests.php">other tests</a></p>
 					<p><a href="eval.html">eval</a></p>
+					<p><a href="DLTasks.php">Download Tasks</a></p>
 					<br>
 				<h4>If you add any new scripts to wiicore.js, please link a button here for easy testing.</h4>
 		</body>
