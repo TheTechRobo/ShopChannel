64b950257f94e9d06ec86da8ff8ba6117aadbc4a
CornierKhan1 <cornierkhan1@disconnect24.xyz>
2018-05-11 17:00:50 +1000

    Fixed DLTasks, credit to Darky.

--- public_html/js/DownloadTasks.js
+++ public_html/js/DownloadTasks.js
@@ -1,7 +1,2 @@
-var addDownload = new addDownloadTask();
-//Adds a download task, needs to be tested.
-var delDownload = new deleteDownloadTask();
-//Deletes a download task, needs to be tested.
-var hasDelDownload = new hasDeletedDLTask();
-//Checks if a download task has been deleted, needs to be tested.
-//All comments in this documents are hypothesized from their titles.
+var DLTask = new wiiDLTask();
+//Download Task majigs.
