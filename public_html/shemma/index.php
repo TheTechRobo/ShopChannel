a36c339186d98fac267688f4e81126cf417ecdf8
Dragos Criste <dragoscriste@gmail.com>
2018-05-23 14:10:35 +0300

    testing

--- public_html/shemma/index.php
+++ public_html/shemma/index.php
@@ -1,9 +1,10 @@
 <html>
-<head>
-<title>Shemma's site</title>
-</head>
-<?php
-session_start();
-echo "test";
-?>
+    <head>
+        <title>Shemma's site</title>
+        <script src="js/main.js"></script>
+        <script src="../js/wiicore.js"></script>
+    </head>
+    <body onload="onstart()">
+        
+    </body>
 </html>
