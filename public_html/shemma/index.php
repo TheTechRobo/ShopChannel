bdc4599906f669833766df628fbcae75dc77d4e5
Dragos Criste <dragoscriste@gmail.com>
2018-05-23 14:17:19 +0300

    testing

--- public_html/shemma/index.php
+++ public_html/shemma/index.php
@@ -1,10 +1,12 @@
 <html>
     <head>
         <title>Shemma's site</title>
+        <link rel="stylesheet" type="text/css" href="css/main.css">
         <script src="js/main.js"></script>
         <script src="../js/wiicore.js"></script>
     </head>
     <body onload="onstart()">
-        
+        <div class="dot" id="line_top">･･･････････････････････････････････････････････････････････････････････････</div>
+        <div class="dot" id="line_bottom">･･･････････････････････････････････････････････････････････････････････････</div>
     </body>
 </html>
