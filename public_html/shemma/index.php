44fcd426175a540b14996281dc2374c5641f3b71
Dragos Criste <dragoscriste@gmail.com>
2018-05-23 14:33:24 +0300

    testing

--- public_html/shemma/index.php
+++ public_html/shemma/index.php
@@ -7,7 +7,10 @@
     </head>
     <body onload="onstart()">
         <div class="dot" id="line_top">･･･････････････････････････････････････････････････････････････････････････</div>
-        <h1>Test</h1>
+        <h1 class="title">(Shemma version of) Open Shop Channel</h1>
+        <div class="main">
+            <img src="https://ih1.redbubble.net/image.483633943.6622/st%2Csmall%2C215x235-pad%2C210x230%2Cf8f8f8.lite-1u4.jpg"></img>
+        </div>
         <div class="dot" id="line_bottom">･･･････････････････････････････････････････････････････････････････････････</div>
     </body>
 </html>
