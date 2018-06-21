f0676389f84b1811b8bda0ee0e8608ebab2916b8
guytwig-PC\guy twig <dhtdht020@gmail.com>
2018-06-21 11:53:21 +0300

    Added Contact Us for Discord in error pages.

--- public_html/404.php
+++ public_html/404.php
@@ -18,7 +18,7 @@ function goBack() {
 </HEAD>
 <BODY>
 <h1 style="color:#009ac7;">Error 404: Not Found.</h1>
-<p>The page you requested cannot be found or no longer exists. Please contact us at our Discord if you believe this is an error.</p>
-<p><img alt="Return Button" id="returnbut" dir="ltr" height="100" src="asset/button/returnA.png" width="200" onmousedown="FP_swapImg(1,1,/*id*/'returnbut',/*url*/'asset/button/returnC.png')" onmouseout="FP_swapImgRestore()" onmouseover="FP_swapImg(1,1,/*id*/'returnbut',/*url*/'asset/button/returnB.png')" onmouseup="FP_swapImgRestore()" class="heading" onclick="FP_goToURL(/*href*/'featured.php')"></p>
+<p>The page you requested cannot be found or no longer exists. Please contact us at our <a href="docs/contactdiscord.php">Discord</a> if you believe this is an error.</p>
+<div align="right"><p><img alt="Return Button" id="returnbut" dir="ltr" height="69" src="asset/button/return1A.png" width="319" onmousedown="FP_swapImg(1,1,/*id*/'returnbut',/*url*/'asset/button/return1C.png')" onmouseout="FP_swapImgRestore()" onmouseover="FP_swapImg(1,1,/*id*/'returnbut',/*url*/'asset/button/return1B.png')" onmouseup="FP_swapImgRestore()" class="heading" onclick="goBack()"></p></div>
 </BODY>
 </HTML>
---
+++ public_html/docs/contactdiscord.php
@@ -0,0 +1,24 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"
+    "http://www.w3.org/TR/html4/strict.dtd">
+<HTML>
+<HEAD>
+    <TITLE>Contact Us - Discord</TITLE>
+    <script type="text/javascript" src="../js/wiicore.js"></script>
+    <script>
+        shop.setWallpaper(2)
+    </script>
+    <script>
+        function goBack() {
+            window.history.back();
+        }
+    </script>
+    <link href="main.css" rel="stylesheet" type="text/css"/>
+    <script type="text/javascript" src="../js/button.js"></script>
+    <script type="text/javascript" src="../js/wiicore.js"></script>
+</HEAD>
+<BODY>
+<h1 style="color:#009ac7;">Contact Us using Discord.</h1>
+<p>To contact us using Discord, Press the plus (+) button at the bottom of your server list, Select "Join" and type: by6mR5N</p>
+<div align="right"><p><img alt="Return Button" id="returnbut" dir="ltr" height="69" src="../asset/button/return1A.png" width="319" onmousedown="FP_swapImg(1,1,/*id*/'returnbut',/*url*/'../asset/button/return1C.png')" onmouseout="FP_swapImgRestore()" onmouseover="FP_swapImg(1,1,/*id*/'returnbut',/*url*/'../asset/button/return1B.png')" onmouseup="FP_swapImgRestore()" class="heading" onclick="goBack()"></p></div>
+</BODY>
+</HTML>
