31489c87ce686fbf455c3c8bd881296272fcf9cd
guytwig-PC\guy twig <dhtdht020@gmail.com>
2018-07-16 10:32:53 +0300

    Using a single div to center the entire page is a better idea.

--- public_html/docs/credits.php
+++ public_html/docs/credits.php
@@ -5,15 +5,17 @@
 <title>Credits</title>
 </head>
 <body>
-<button onClick="window.history.back();" align="center">Back</button>
-<h1 align="center">About OSC</h1>
-<li align="center">Backend - dhtdht020, DarkPheonix </li>
-<li align="center">Frontend - CornierKhan1</li>
-<li align="center">Dynamic Banners - Larsenv</li>
-<li align="center">Development Assistance - Snoot</li>
-<div></div>
-<p align="center">Open Shop Channel for the Nintendo Wii - Copyright (C) 2018 Open Shop Channel Team.</p>
-<p align="center">This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.</p>
-<p align="center">This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details. You should have received a copy of the GNU Affero General Public License along with this program. If not, see http://www.gnu.org/licenses/.</p>
+    <button onClick="window.history.back();" align="center">Back</button>
+<div align="center">
+    <h1>About OSC</h1>
+    <li>Backend - dhtdht020, DarkPheonix </li>
+    <li>Frontend - CornierKhan1</li>
+    <li>Dynamic Banners - Larsenv</li>
+    <li>Development Assistance - Snoot</li>
+    <div></div>
+    <p>Open Shop Channel for the Nintendo Wii - Copyright (C) 2018 Open Shop Channel Team.</p>
+    <p>This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.</p>
+    <p>This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details. You should have received a copy of the GNU Affero General Public License along with this program. If not, see http://www.gnu.org/licenses/.</p>
+</div>
 </body>
 </html>
