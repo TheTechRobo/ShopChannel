2f46421890e7265d2be29e16c62ae1d622edc507
Guy Twig <dhtdht020@gmail.com>
2018-08-13 12:40:16 +0300

    Replaced basic "Return" button with styled.

--- public_html/docs/credits.php
+++ public_html/docs/credits.php
@@ -3,9 +3,17 @@
 <head>
 <meta charset="utf-8">
 <title>Credits</title>
+    <script type="text/javascript" src="../js/wiicore.js"></script>
+    <link href="../main.css" rel="stylesheet" type="text/css"/>
+    <script type="text/javascript" src="../js/button.js"></script>
+    <script type="text/javascript" src="../js/wiicore.js"></script>
+    <script>
+        function goBack() {
+            window.history.back();
+        }
+    </script>
 </head>
 <body>
-    <button onClick="window.history.back();" align="center">Back</button>
 <div align="center">
     <h1>About OSC</h1>
     <li>Backend - dhtdht020, DarkPheonix </li>
@@ -16,6 +24,7 @@
     <p>Open Shop Channel for the Nintendo Wii - Copyright (C) 2018 Open Shop Channel Team.</p>
     <p>This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.</p>
     <p>This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details. You should have received a copy of the GNU Affero General Public License along with this program. If not, see http://www.gnu.org/licenses/.</p>
+    <div align="right"><p><img alt="Return Button" id="returnbut" dir="ltr" height="69" src="../asset/button/return1A.png" width="319" onmousedown="FP_swapImg(1,1,/*id*/'returnbut',/*url*/'../asset/button/return1C.png')" onmouseout="FP_swapImgRestore()" onmouseover="FP_swapImg(1,1,/*id*/'returnbut',/*url*/'../asset/button/return1B.png')" onmouseup="FP_swapImgRestore()" class="heading" onclick="goBack()"></p></div>
 </div>
 </body>
 </html>
