b2a81b112e021e12ed8f48ec2801cd73ecaa9183
CornierKhan1 <jordyalexlyall@gmail.com>
2018-10-02 00:50:36 +1000

    s h h h

--- public_html/index.php
+++ public_html/index.php
@@ -3,6 +3,109 @@
 <HTML>
 	<HEAD>
 	    <TITLE>OSC Testing Ground</TITLE>
+		<script type="text/javascript">
+			function checkRegistration()
+{
+    var progress = ec.checkRegistration ();
+    opName = "Check Registration";
+    opDesc = "Checking Registration";
+    finishOp(opName, opDesc, progress, "onCheckRegistrationDone");
+}
+
+function onCheckRegistrationDone (progress)
+{
+    showResult (progress, opName, opDesc);
+    if (progress.status >= 0) {
+        var r = ec.getDeviceInfo ();
+        // If not registered - set loyalty system and show terms, call register
+        // If transfer pending - show "Confirm Transfer", call transfer
+        // If transfer complete - call transfer to get account id (show status)
+        // If registered - call register to get account id (show status)
+        //trace("device id=" + r.accountId + " status=" + r.registrationStatus);
+        switch (r.registrationStatus) {
+          case 'U': // Not registered
+                // Ask user to register
+
+                if (useSyncRegistration) {
+                    if (r.accountId == EC_INVALID_ACCOUNT_ID) {
+                        // Ask user to register
+                        trace("Ask the user to register - U");
+                        replacePage('L_03.jsp');
+                    } else {
+                        // Synchronizes registration info
+                        trace("Get registration info - U");
+                        showRegister("getRegistrationInfo");
+                    }
+                } else {
+                     // Ask user to register
+                     trace("Ask the user to register - U");
+                        replacePage('L_03.jsp');
+                }
+                break;
+            case 'R': // Registered
+                trace("Get registration info - R");
+                showRegister("getRegistrationInfo");
+                break;
+            case 'P': // Transfer pending
+                trace("Transfer pending - P");
+                showRegister("performPendingTransfer");
+                break;
+            case 'T':
+                trace("Get transfer info - T");
+                showRegister("getTransferInfo");
+                break;
+            default:
+                var errorStr = "Unknown registration status: " + r.registrationStatus;
+                trace(errorStr);;
+                setConnectingToServer(false);               
+                setUnderButton(true, shop.menuBtn, "javascript:showWiiMenu()", "wiiSelectSound();");
+                hideElement("text02-01");
+                var elem = document.getElementById("errorCodeText");
+                setErrorHtmlMsg(elem, OSS_INVALID_REGISTRATION_STATUS, "The Wii Number you entered is incorrect.<BR><BR>Visit support.nintendo.com for assistance.");
+                break;              
+        }
+    } else {
+        setConnectingToServer(false);
+        setUnderButton(true, shop.menuBtn, "javascript:showWiiMenu()", "wiiSelectSound();");
+        // Show error message and code
+        hideElement("text02-01");
+        var elem = document.getElementById("errorCodeText");
+        setErrorHtml(elem, progress);
+    }
+}
+
+// Check if device is registered
+function checkRegistered()
+{
+    setUnderButton(false);
+    var r = ec.getDeviceInfo ();
+    // Probably we can delete the line below.
+    var checkRegisterNeeded = isCheckRegisterNeeded();
+    var weakToken = ec.getWeakToken();
+    trace("AccountId=" + r.accountId + " token=" + weakToken + " Country=" + r.country + " a/c Country=" + r.accountCountry);
+    trace("DeviceCode=" + r.deviceCode + " a/c DeviceCode=" + r.accountDeviceCode);
+        
+    if (!checkRegisterNeeded) {
+        checkRegisterNeeded = (r.accountId == EC_INVALID_ACCOUNT_ID || !weakToken);
+    }
+        
+    if (checkRegisterNeeded) {
+        checkRegistration();
+    } else {
+        if (r.country != r.accountCountry) {
+            replacePage("L_01.jsp?origCountry=" + r.accountCountry);
+        } else if ((r.accountDeviceCode != undefined)
+                    && (r.accountDeviceCode != null)
+                    && (r.accountDeviceCode != "") 
+                    && (r.deviceCode != r.accountDeviceCode)) {
+            trace("Re-registering because device code is out of sync: old " + r.accountDeviceCode + ", new " + r.deviceCode);
+            showRegister("register");
+        } else {
+            showInitialPage(true);
+        }
+    }
+}
+		</script>
 	</HEAD>
 		<BODY>
 		Hello, welcome to this HTML 4.01 document.
@@ -16,10 +119,7 @@
             <p><a href="/error/list.php">Error List</a></p>
 			<p><a href="debug.php">debugmii</a></p>
 			<p><button onmousedown="sound.playBGM()">play thy bgm</button></p>
-			<p><button onmousedown="ec.setWebSvcUrls('https://soapy.oscwii.org/', 'https://soapy.oscwii.org');function checkRegistration()
-{
-    var progress = ec.checkRegistration ();
-    finishOp('owo', 'uwu', progress, 'onCheckRegistrationDone');}checkRegistration();">Check Registration!?</button></p>
+			<p><button onmousedown="">Check Registration!?</button></p>
 			<p id="test" style="display: none;"></p>
         <p>Open Shop Channel for the Nintendo Wii
             Copyright (C) 2018  Open Shop Channel Team</p>
