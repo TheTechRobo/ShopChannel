b2c95edbb1c6d6d7c1d9445a112c0a1330cfbc84
CornierKhan1 <jordyalexlyall@gmail.com>
2018-10-05 15:30:37 +1000

    smh

--- public_html/js/wiicore.js
+++ public_html/js/wiicore.js
@@ -1,6 +1,16 @@
 //This file is for quick access, simply use <script src="js/wiicore.js"></script> to load it into any page on public_html.
 var toLoad = ["wiiShop", "wiiKeyboard", "wiiSound", "wiiECommerceInterface", "DownloadTasks", "SD", "NWC24", "mii"]
 var newScriptLol;
+var opName;
+var opDesc;
+var ossPath = "";
+var secureOssPath = "";
+var ecTimeout = null;
+var ecProgressInterval = 1000; // Timeout for checking progress in milliseconds
+var useSyncRegistration = false;
+var nwcBusyCheckTimer;
+var nwcOnSendMailDone = null;
+
 for (var ok = 0; ok < toLoad.length; ok++) {
     newScriptLol = document.createElement("script");
 	newScriptLol.type = "text/javascript";
