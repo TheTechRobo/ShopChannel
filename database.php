cda1f8dd3bbc6c86da73159fc89c72e0ef31d7ff
CornierKhan1 <jordyalexlyall@gmail.com>
2018-11-30 19:14:18 +1100

    Billy provided assistance with PhP. (Yay)

---
+++ database.php
@@ -0,0 +1,29 @@
+// Courtesy of Kaeru.
+error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT);
+//error_reporting(E_ALL);
+mb_http_output('UTF-8');
+
+global $db;
+
+function connectMySQL(){
+    global $db;
+
+    if(!$db) $db = new mysqli('127.0.0.1', 'USER', 'PASS', 'DB');
+    return $db;
+}
+
+function new_get_result(&$stmt) {
+    $result = array();
+    $stmt->store_result();
+    for($i = 0; $i < $stmt->num_rows; $i++) {
+        $meta = $stmt->result_metadata();
+        $params = array();
+        while ( $field = $meta->fetch_field() ) {
+            $params[] = &$result[ $i ][ $field->name ];
+        }
+        call_user_func_array(array($stmt, 'bind_result'), $params);
+        $stmt->fetch();
+    }
+    $stmt->close();
+    return $result;
+}
