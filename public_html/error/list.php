0e5ba610b97434880c859ac79491128d1eaabf62
Guy Twig <dhtdht020@gmail.com>
2019-01-30 21:38:52 +0200

    We always find our way back home

--- public_html/error/list.php
+++ public_html/error/list.php
@@ -7,6 +7,7 @@
     <BODY>
         <p><a href="404.php">404 - Not Found</a></p>
         <p><a href="403.php">403 - Forbidden</a></p>
+        <p><button onclick="window.history.back()">bring me back home daddy</button></p>
         <script type="text/javascript" src="../js/wiicore.js"></script>
     </BODY>
 </HTML>
