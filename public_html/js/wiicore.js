f79bce36aeab076fc4cabc8411a5e1edf0766651
DarkPhoenix10 <dark@darkok.xyz>
2019-02-01 10:46:33 +0000

    add button.js to wiicore, and reduce file size
    hopefully it works, can't test it at the moment lol

--- public_html/js/wiicore.js
+++ public_html/js/wiicore.js
@@ -1,19 +1,18 @@
 //This file is for quick access, simply use <script src="js/wiicore.js"></script> to load it into any page on public_html.
-var toLoad = ["wiiShop", "wiiKeyboard", "wiiSound", "wiiECommerceInterface", "DownloadTasks", "SD", "NWC24", "mii"]
-var newScriptLol;
+var toLoad=["wiiShop","wiiKeyboard","wiiSound","wiiECommerceInterface","DownloadTasks","SD","NWC24","mii","button"]
 var opName;
 var opDesc;
-var ossPath = "";
-var secureOssPath = "";
-var ecTimeout = null;
-var ecProgressInterval = 1000; // Timeout for checking progress in milliseconds
-var useSyncRegistration = false;
+var ossPath="";
+var secureOssPath="";
+var ecTimeout=null;
+var ecProgressInterval=1000; // Timeout for checking progress in milliseconds
+var useSyncRegistration=false;
 var nwcBusyCheckTimer;
-var nwcOnSendMailDone = null;
+var nwcOnSendMailDone=null;
 
-for (var ok = 0; ok < toLoad.length; ok++) {
-	newScriptLol = document.createElement("script");
-	newScriptLol.type = "text/javascript";
-	newScriptLol.src = "https://" + location.host + "/js/" + toLoad[ok] + ".js";
-	document.getElementsByTagName("head")[0].appendChild(newScriptLol);
+for(var k=0;k<toLoad.length;k++){
+    	var s=document.createElement("script");
+	s.type="text/javascript";
+	s.src="https://"+location.host+"/js/"+toLoad[k]+".js";
+	document.getElementsByTagName("head")[0].appendChild(s);
 }
