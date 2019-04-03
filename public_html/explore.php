6ef3b3f2ec8e4635e013c6b6d5ab5b2398902e93
Rasmus <rasmusbamse@hotmail.com>
2019-04-03 20:04:24 +0200

    Implement fetching title data from a MySQL database.

--- public_html/explore.php
+++ public_html/explore.php
@@ -1,35 +1,35 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-    <meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
-    <title>Explore</title>
-    <style type="text/css">
-        .heading {
-            font-size: 32px;
-            font-weight: 900;
-            font-style: normal;
-            font-variant: normal;
-            text-transform: none;
-            color: #000000;
-            text-align:center
-        }
-        .highlighted {
-            font-size: 16px;
-            font-weight: 200;
-            font-style: normal;
-            font-variant: normal;
-            text-transform: none;
-            color: #FFFFFF;
-            background-color: #34BEED;
-        }
-        .default {
-            font-size: 16px;
-            font-weight: 200;
-            font-style: normal;
-            font-variant: normal;
-            text-transform: none;
-            color: #000000;
-        }
+	<meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
+	<title>Explore</title>
+	<style type="text/css">
+		.heading {
+			font-size: 32px;
+			font-weight: 900;
+			font-style: normal;
+			font-variant: normal;
+			text-transform: none;
+			color: #000000;
+			text-align:center
+		}
+		.highlighted {
+			font-size: 16px;
+			font-weight: 200;
+			font-style: normal;
+			font-variant: normal;
+			text-transform: none;
+			color: #FFFFFF;
+			background-color: #34BEED;
+		}
+		.default {
+			font-size: 16px;
+			font-weight: 200;
+			font-style: normal;
+			font-variant: normal;
+			text-transform: none;
+			color: #000000;
+		}
 		.imgShadow {
 			position: absolute;
 			top: 109.5px;
@@ -78,45 +78,45 @@
 			position: absolute;
 			left: 423px;
 		}
-    </style>
-    <script type="text/javascript" src="js/wiicore.js"></script>
-    <script type="text/javascript">
-        <!--
-        function FP_swapImg() {//v1.0
-            var doc=document,args=arguments,elm,n; doc.$imgSwaps=new Array(); for(n=2; n<args.length;
-                                                                                  n+=2) { elm=FP_getObjectByID(args[n]); if(elm) { doc.$imgSwaps[doc.$imgSwaps.length]=elm;
-                elm.$src=elm.src; elm.src=args[n+1]; } }
-        }
-        function FP_preloadImgs() {//v1.0
-            var d=document,a=arguments; if(!d.FP_imgs) d.FP_imgs=new Array();
-            for(var i=0; i<a.length; i++) { d.FP_imgs[i]=new Image; d.FP_imgs[i].src=a[i]; }
-        }
-        function FP_getObjectByID(id,o) {//v1.0
-            var c,el,els,f,m,n; if(!o)o=document; if(o.getElementById) el=o.getElementById(id);
-            else if(o.layers) c=o.layers; else if(o.all) el=o.all[id]; if(el) return el;
-            if(o.id==id || o.name==id) return o; if(o.childNodes) c=o.childNodes; if(c)
-                for(n=0; n<c.length; n++) { el=FP_getObjectByID(id,c[n]); if(el) return el; }
-            f=o.forms; if(f) for(n=0; n<f.length; n++) { els=f[n].elements;
-                for(m=0; m<els.length; m++){ el=FP_getObjectByID(id,els[n]); if(el) return el; } }
-            return null;
-        }
-        function FP_swapImgRestore() {//v1.0
-            var doc=document,i; if(doc.$imgSwaps) { for(i=0;i<doc.$imgSwaps.length;i++) {
-                var elm=doc.$imgSwaps[i]; if(elm) { elm.src=elm.$src; elm.$src=null; } }
-                doc.$imgSwaps=null; }
-        }
-        function FP_goToURL(url) {//v1.0
-            window.location=url;
-        }
-        shop.setWallpaper(2);
-        // -->
-    </script>
+	</style>
+	<script type="text/javascript" src="js/wiicore.js"></script>
+	<script type="text/javascript">
+		<!--
+		function FP_swapImg() {//v1.0
+			var doc=document,args=arguments,elm,n; doc.$imgSwaps=new Array(); for(n=2; n<args.length;
+																				  n+=2) { elm=FP_getObjectByID(args[n]); if(elm) { doc.$imgSwaps[doc.$imgSwaps.length]=elm;
+				elm.$src=elm.src; elm.src=args[n+1]; } }
+		}
+		function FP_preloadImgs() {//v1.0
+			var d=document,a=arguments; if(!d.FP_imgs) d.FP_imgs=new Array();
+			for(var i=0; i<a.length; i++) { d.FP_imgs[i]=new Image; d.FP_imgs[i].src=a[i]; }
+		}
+		function FP_getObjectByID(id,o) {//v1.0
+			var c,el,els,f,m,n; if(!o)o=document; if(o.getElementById) el=o.getElementById(id);
+			else if(o.layers) c=o.layers; else if(o.all) el=o.all[id]; if(el) return el;
+			if(o.id==id || o.name==id) return o; if(o.childNodes) c=o.childNodes; if(c)
+				for(n=0; n<c.length; n++) { el=FP_getObjectByID(id,c[n]); if(el) return el; }
+			f=o.forms; if(f) for(n=0; n<f.length; n++) { els=f[n].elements;
+				for(m=0; m<els.length; m++){ el=FP_getObjectByID(id,els[n]); if(el) return el; } }
+			return null;
+		}
+		function FP_swapImgRestore() {//v1.0
+			var doc=document,i; if(doc.$imgSwaps) { for(i=0;i<doc.$imgSwaps.length;i++) {
+				var elm=doc.$imgSwaps[i]; if(elm) { elm.src=elm.$src; elm.$src=null; } }
+				doc.$imgSwaps=null; }
+		}
+		function FP_goToURL(url) {//v1.0
+			window.location=url;
+		}
+		shop.setWallpaper(2);
+		// -->
+	</script>
 </head>
 <body onload="FP_preloadImgs(/*url*/'asset/button/open2.png',/*url*/'asset/button/open3.png',/*url*/'asset/section/clubspotB.png',/*url*/'asset/section/clubspotC.png',/*url*/'asset/section/patchesB.png',/*url*/'asset/section/patchesC.png',/*url*/'asset/section/homebrewB.png',/*url*/'asset/section/homebrewC.png',/*url*/'asset/button/returnB.png',/*url*/'asset/button/returnC.png')">
 
 <p class="heading"><img src="asset/logo/bluelogo.png" width="32" height="32"> Open Shop Channel <span class="highlighted">Explore</span>
-    <span class="default">&nbsp;&nbsp;&nbsp;&nbsp; </span>
-    <img alt="About" id="aboutbut" dir="ltr" height="38" src="asset/button/open.png" width="36" onmousedown="FP_swapImg(1,1,/*id*/'aboutbut',/*url*/'asset/button/open3.png')" onmouseout="FP_swapImgRestore()" onmouseover="FP_swapImg(1,1,/*id*/'aboutbut',/*url*/'asset/button/open2.png')" onmouseup="FP_swapImgRestore()" class="heading" onclick="FP_goToURL('docs/credits.php')">&nbsp;
+	<span class="default">&nbsp;&nbsp;&nbsp;&nbsp; </span>
+	<img alt="About" id="aboutbut" dir="ltr" height="38" src="asset/button/open.png" width="36" onmousedown="FP_swapImg(1,1,/*id*/'aboutbut',/*url*/'asset/button/open3.png')" onmouseout="FP_swapImgRestore()" onmouseover="FP_swapImg(1,1,/*id*/'aboutbut',/*url*/'asset/button/open2.png')" onmouseup="FP_swapImgRestore()" class="heading" onclick="FP_goToURL('docs/credits.php')">&nbsp;
 </p>
 
 <!--For the sake of order, stylize Buttons with the heading class in main.css.-->
@@ -125,13 +125,13 @@
 <div class="imgShadow" style="left: 361px;"></div>
 	
 <p class="heading">
-    <!--Sections-->	
-    <img alt="Club Spot" id="clubspotbut" dir="ltr" height="100" src="asset/section/clubspotA.png" width="100" onmousedown="FP_swapImg(1,1,/*id*/'clubspotbut',/*url*/'asset/section/clubspotC.png')" onmouseout="FP_swapImgRestore(); document.getElementById('HoverClubSpot').style.visibility='hidden';" onmouseover="FP_swapImg(1,1,/*id*/'clubspotbut',/*url*/'asset/section/clubspotB.png'); document.getElementById('HoverClubSpot').style.visibility='visible';" onmouseup="FP_swapImgRestore()" class="heading" onclick="FP_goToURL('clubspot/index.php')">
-    <img alt="Patches" id="patchesbut" dir="ltr" height="100" src="asset/section/patchesA.png" width="100" onmousedown="FP_swapImg(1,1,/*id*/'patchesbut',/*url*/'asset/section/patchesC.png')" onmouseout="FP_swapImgRestore(); document.getElementById('HoverPatch').style.visibility='hidden';" onmouseover="FP_swapImg(1,1,/*id*/'patchesbut',/*url*/'asset/section/patchesB.png'); document.getElementById('HoverPatch').style.visibility='visible';" onmouseup="FP_swapImgRestore()" class="heading" onclick="FP_goToURL('patches/list_warn.php');">
-    <img alt="Homebrew" id="homebrewbut" dir="ltr" height="100" src="asset/section/homebrewA.png" width="100" onmousedown="FP_swapImg(1,1,/*id*/'homebrewbut',/*url*/'asset/section/homebrewC.png')" onmouseout="FP_swapImgRestore(); document.getElementById('HoverHomebrew').style.visibility='hidden';" onmouseover="FP_swapImg(1,1,/*id*/'homebrewbut',/*url*/'asset/section/homebrewB.png'); document.getElementById('HoverHomebrew').style.visibility='visible';" onmouseup="FP_swapImgRestore()" class="heading" onclick="FP_goToURL(/*href*/'homebrew/list.php');">
-    </img></p>
+	<!--Sections-->	
+	<img alt="Club Spot" id="clubspotbut" dir="ltr" height="100" src="asset/section/clubspotA.png" width="100" onmousedown="FP_swapImg(1,1,/*id*/'clubspotbut',/*url*/'asset/section/clubspotC.png')" onmouseout="FP_swapImgRestore(); document.getElementById('HoverClubSpot').style.visibility='hidden';" onmouseover="FP_swapImg(1,1,/*id*/'clubspotbut',/*url*/'asset/section/clubspotB.png'); document.getElementById('HoverClubSpot').style.visibility='visible';" onmouseup="FP_swapImgRestore()" class="heading" onclick="FP_goToURL('clubspot/index.php')">
+	<img alt="Patches" id="patchesbut" dir="ltr" height="100" src="asset/section/patchesA.png" width="100" onmousedown="FP_swapImg(1,1,/*id*/'patchesbut',/*url*/'asset/section/patchesC.png')" onmouseout="FP_swapImgRestore(); document.getElementById('HoverPatch').style.visibility='hidden';" onmouseover="FP_swapImg(1,1,/*id*/'patchesbut',/*url*/'asset/section/patchesB.png'); document.getElementById('HoverPatch').style.visibility='visible';" onmouseup="FP_swapImgRestore()" class="heading" onclick="FP_goToURL('patches/list_warn.php');">
+	<img alt="Homebrew" id="homebrewbut" dir="ltr" height="100" src="asset/section/homebrewA.png" width="100" onmousedown="FP_swapImg(1,1,/*id*/'homebrewbut',/*url*/'asset/section/homebrewC.png')" onmouseout="FP_swapImgRestore(); document.getElementById('HoverHomebrew').style.visibility='hidden';" onmouseover="FP_swapImg(1,1,/*id*/'homebrewbut',/*url*/'asset/section/homebrewB.png'); document.getElementById('HoverHomebrew').style.visibility='visible';" onmouseup="FP_swapImgRestore()" class="heading" onclick="FP_goToURL(/*href*/'list.php?category=1');">
+	</img></p>
 <p class="heading">
-    <img alt="Return Button" id="returnbut" dir="ltr" height="69" src="asset/button/return1A.png" width="319" onmousedown="FP_swapImg(1,1,/*id*/'returnbut',/*url*/'asset/button/return1C.png')" onmouseout="FP_swapImgRestore()" onmouseover="FP_swapImg(1,1,/*id*/'returnbut',/*url*/'asset/button/return1B.png')" onmouseup="FP_swapImgRestore()" class="heading" onclick="FP_goToURL(/*href*/'index.php')">
+	<img alt="Return Button" id="returnbut" dir="ltr" height="69" src="asset/button/return1A.png" width="319" onmousedown="FP_swapImg(1,1,/*id*/'returnbut',/*url*/'asset/button/return1C.png')" onmouseout="FP_swapImgRestore()" onmouseover="FP_swapImg(1,1,/*id*/'returnbut',/*url*/'asset/button/return1B.png')" onmouseup="FP_swapImgRestore()" class="heading" onclick="FP_goToURL(/*href*/'index.php')">
 </p>
 
 <!--Information that appears on hover-->
--- public_html/prodinfo.php
+++ public_html/prodinfo.php
@@ -1,43 +1,54 @@
+<?php
+require('lib/functions.php');
+
+$id = (isset($_GET['id']) ? $_GET['id'] : 1);
+
+$titledata = fetch("SELECT * FROM titles WHERE titleid = ?", [$id]);
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <meta charset="utf-8">
 
 <head>
-<link href="../../main.css" rel="stylesheet" type="text/css" />
+<link href="main.css" rel="stylesheet" type="text/css" />
 </head>
 
 <body>
 		<!--Header-->
-		<a href="../../index.php" style="text-decoration: none;">
+		<a href="index.php" style="text-decoration: none;">
 			<div class="textOpenShop"><div><div><div>
 				<h1 class="textOpenShopH">Details</h1>
 			</div></div></div></div>
 		</a>
 		<h6 class="dots" style="margin-top: 12px; left: 5px;">• • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • </h6>
 		
-		<img src="../../asset/bg/details.png" class="infoBG">
+		<img src="asset/bg/details.png" class="infoBG">
 		
 		<!--info section-->
-		<p class="textPlatform">Type</p>
+		<p class="textPlatform"><?=$categories[$titledata['categoryid']] ?></p>
 		<p class="textNormal">
-			Released MM/DD/YYYY<br>
-			Creator Name<br>
-			Genre
+			Released <?=date('m/d/Y', $titledata['date'])?><br>
+			<?=$titledata['author'] ?><br>
+			<?=$titledata['genre'] ?>
 		</p>
-		<p class="textNormal" style="left: 400px;">For # player(s)</p>
-			<p class="textTitle">Product Name Here</p>
-		<img src="../../asset/temporary/icon.png" class="imageScreenshot"><!--Product icon here-->
+		<p class="textNormal" style="left: 400px;">
+			For <?=$titledata['players'] ?> player<?=($titledata['players'] != 1 ? 's' : '') ?>
+		</p>
+			<p class="textTitle"><?=$titledata['name'] ?></p>
+		<img src="download/<?=$titledata['titleid'] ?>/icon.png" class="imageScreenshot"><!--Product icon here-->
 		
 		<h6 class="dots" style="margin-top: 316px; left: 5px;">• • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • </h6>
 		<br>
 		
 		<!--Download button-->
 		<a href=""> <!--Link to download page here-->
-			<img src="../../asset/button/download2SIZE.png" class="buttonDownload">
+			<img src="asset/button/download2SIZE.png" class="buttonDownload">
 		</a>
 		
 		<!--Footer buttons-->
-		<img src="../../asset/button/return1A.png" class="returnButton" onmouseover="this.src='../../asset/button/return1B.png'" onmouseout="this.src='../../asset/button/return1A.png'" onmousedown="this.src='../../asset/button/return1C.png'" onclick="window.history.back()">
+		<a href=""> <!--Previous page here-->
+			<img src="asset/button/return1A.png" class="returnButton" onmouseover="this.src='asset/button/return1B.png'" onmouseout="this.src='asset/button/return1A.png'" onmousedown="this.src='asset/button/return1C.png'">
+		</a>
 	</body>
 
 </html>
