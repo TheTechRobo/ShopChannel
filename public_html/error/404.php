3f034838f581be3755c3796a765d02dfe429bbad
Scruto <51213816+Scruto@users.noreply.github.com>
2019-06-05 13:34:13 +0100

    Changed the 404 Message slightly
    Made the message more user-friendly

--- public_html/error/404.php
+++ public_html/error/404.php
@@ -18,7 +18,7 @@
     </HEAD>
     <BODY>
         <h1 style="color:#009ac7;">Error 404: Not Found.</h1>
-        <p>The page you requested cannot be found or no longer exists. Please contact us at our <a href="../docs/contactdiscord.php">Discord</a> if you believe this is an error.</p>
+        <p>The page you requested cannot be found or no longer exists. If you believe this is an error, visit <a href="../docs/contactdiscord.php">this page</a> to learn how to join the OSC Discord Server.</p>
         <div align="right"><p><img alt="Return Button" id="returnbut" dir="ltr" height="69" src="../asset/button/return1A.png" width="319" onmousedown="FP_swapImg(1,1,/*id*/'returnbut',/*url*/'../asset/button/return1C.png')" onmouseout="FP_swapImgRestore()" onmouseover="FP_swapImg(1,1,/*id*/'returnbut',/*url*/'../asset/button/return1B.png')" onmouseup="FP_swapImgRestore()" class="heading" onclick="goBack()"></p></div>
     </BODY>
 </HTML>
