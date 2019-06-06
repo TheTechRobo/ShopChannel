69e24882fcf19401723015691f379f3bcb607762
Scruto <51213816+Scruto@users.noreply.github.com>
2019-06-06 13:50:45 +0100

    An even better 403 message
    Changed the error message again

--- public_html/error/403.php
+++ public_html/error/403.php
@@ -18,7 +18,7 @@
     </HEAD>
     <BODY>
         <h1 style="color:#009ac7;">Error 403: Forbidden.</h1>
-        <p>You do not have authorization to visit this section of the OSC. Please contact us at our <a href="../docs/contactdiscord.php">Discord</a> if you believe this is an error.</p>
+        <p>You do not have authorization to visit this page. Please contact us at our <a href="../docs/contactdiscord.php">Discord</a> if you believe this is a mistake on our end!</p>
         <div align="right"><p><img alt="Return Button" id="returnbut" dir="ltr" height="69" src="../asset/button/return1A.png" width="319" onmousedown="FP_swapImg(1,1,/*id*/'returnbut',/*url*/'../asset/button/return1C.png')" onmouseout="FP_swapImgRestore()" onmouseover="FP_swapImg(1,1,/*id*/'returnbut',/*url*/'../asset/button/return1B.png')" onmouseup="FP_swapImgRestore()" class="heading" onclick="goBack()"></p></div>
         </BODY>
 </HTML>
