6c54a8d5c2526ccbbaedd1870f496cf55fff2d22
SoulRepo <aedencloud@icloud.com>
2019-06-05 20:38:19 -0400

    Changed debug button text

--- public_html/index.php
+++ public_html/index.php
@@ -55,7 +55,7 @@
 <p>
 <p class="heading"><img alt="Explore Button" id="explore" dir="ltr" height="100" src="asset/button/exploreA.png" width="200" onmousedown="FP_swapImg(1,1,/*id*/'explore',/*url*/'asset/button/exploreC.png')" onmouseout="FP_swapImgRestore()" onmouseover="FP_swapImg(1,1,/*id*/'explore',/*url*/'asset/button/exploreB.png');" onmouseup="FP_swapImgRestore()" class="heading" onclick="FP_goToURL(/*href*/'explore.php');"></p>
 <p class="heading"><img src="asset/logo/LogoText.png" width="242" height="100.5"></p>
-<p><a href="toolbox.php">i wanna be debugging</a></p>
+<p><a href="toolbox.php">Debugger for devs</a></p>
 <p align="right">Copyright (C) 2018-2019 Open Shop Channel Team</p>
 </p>
 <p id="test" style="display: none;"></p>
