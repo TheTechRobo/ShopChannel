5ebfa47e6dc92f3cd1cc306907be647ede1157a9
SoulRepo <aedencloud@icloud.com>
2019-06-06 16:28:15 -0400

    those dang errors

--- public_html/error/list.php
+++ public_html/error/list.php
@@ -2,12 +2,13 @@
     "http://www.w3.org/TR/html4/strict.dtd">
 <HTML>
     <HEAD>
-        <TITLE>Error List</TITLE>
+        <TITLE>Error Test</TITLE>
     </HEAD>
     <BODY>
+        <p>Test if those dang errors work.</p>
         <p><a href="404.php">404 - Not Found</a></p>
         <p><a href="403.php">403 - Forbidden</a></p>
-        <p><button onclick="window.history.back()">bring me back home daddy</button></p>
+        <p><button onclick="window.history.back()">Go back</button></p>
         <script type="text/javascript" src="../js/wiicore.js"></script>
     </BODY>
 </HTML>
